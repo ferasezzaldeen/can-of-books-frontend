e07113402288dfbbc02a82dd9a2ca60e6d0beef6
FerasSadTawfiqEzz-aldeen <Ferasezaldeen@gmail.com>
2021-06-22 19:31:38 +0300

    finish lab13

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,21 +2,38 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { withAuth0 } from '@auth0/auth0-react';
 import MyFavoriteBooks from './MyFavoriteBooks';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 class BestBooks extends React.Component {
 
     render() {
         return (<div>
-            {this.props.booksData.map(item => {
-                return (<><div>
+            <CardGroup >
+                {this.props.booksData.map((item, idx) => {
+                    return (<><div key={idx}>
 
-                    <p>name of the book : {item.name}</p>
-                    <p>description of the book : {item.description}</p>
-                    <p>state of the book : {item.status}</p>
 
-                </div> </>)
+                        <Card >
 
-            })}
+                            <Card.Body style={{ width: '18rem'}}   >
+                                <Card.Title>  <p>{item.name}</p></Card.Title>
+                                <Card.Text>
+                                    <p>description: {item.description}</p>
+                                </Card.Text>
+                                <Card.Text> <p>state: {item.status}</p>  </Card.Text>
+                                <Button variant="primary"  onClick={()=>this.props.deletBOOK(idx)} >
+                                    delet this book
+                                </Button>
+                            </Card.Body>
 
+                        </Card>
+
+                    </div> </>)
+
+                })}
+            </CardGroup>
 
         </div>)
     }
--- src/MyFavoriteBooks.js
+++ src/MyFavoriteBooks.js
@@ -5,6 +5,11 @@ import './myFavoriteBooks.css';
 import axios from 'axios';
 import { withAuth0 } from '@auth0/auth0-react';
 import BestBooks from './BestBooks';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Forms from './Forms';
+
 
 
 
@@ -16,6 +21,7 @@ class MyFavoriteBooks extends React.Component {
       numOfBooks: 0,
       booksData: [],
       showbooks: false,
+      show: false,
     }
 
 
@@ -29,25 +35,86 @@ class MyFavoriteBooks extends React.Component {
     let bookURL = process.env.REACT_APP_SERVER;
     let url = `${bookURL}books?email=${email}`
     let book = await axios.get(url);
-    console.log('this is the booksdata',this.state.booksData);
+    console.log('this is the booksdata', this.state.booksData);
     console.log(book);
     this.setState({
       booksData: book.data,
       showbooks: true
     })
-    console.log('this is the booksdata',this.state.booksData);
+    console.log('this is the booksdata', this.state.booksData);
+  }
 
+
+  addBook= async (event)=>{
+    const { user } = this.props.auth0;
+    event.preventDefault();
+    const bookInfo={
+      name:event.target.name.value,
+      description:event.target.description.value,
+      status:event.target.status.value,
+      email:user.email,
+    };
+    console.log('add book function is working')
+    let bookURL = process.env.REACT_APP_SERVER;
+    let url = `${bookURL}addBooks`
+    console.log('url function is working')
+    console.log( `${bookURL}addBooks?${this.props.auth0.user.email}`)
+    let newBook=await axios.post(url,bookInfo);
+    console.log('axios function is working')
+
+    this.setState({
+      booksData: newBook.data,
+      showbooks:true,
+    })
+
+  }
+
+  deletBOOK= async(idx)=>{
+    const email=this.props.auth0.user.email;
+    const bookInfo={
+      email:email,
+      index:idx,
+    };
+    console.log(email);
+    console.log(bookInfo);
+    let bookURL = process.env.REACT_APP_SERVER;
+    let url = `${bookURL}delBooks`
+    let newBook=await axios.delete(url,{params:bookInfo});
+
+    this.setState({
+      booksData: newBook.data,
+      showbooks:true,
+    })
+
+  }
+
+
+
+  handleShow = () => {
+    this.setState({
+      show: true
+    })
+  }
+  handleClose = () => {
+    this.setState({
+      show: false
+    })
   }
 
   render() {
     return (
       <Jumbotron>
         <h1>My Favorite Books</h1>
+        <Button variant="primary" onClick={this.handleShow}>
+          Add a book
+        </Button>
+        <Forms handleClose={this.handleClose} addBook={this.addBook} show={this.state.show}  />
         <p>
           This is a collection of my favorite books
         </p>
+
         <p>{this.state.booksData.name}</p>
-        { this.state.showbooks && <BestBooks booksData={this.state.booksData} showbooks={this.state.showbooks} />}
+        {this.state.showbooks && <BestBooks booksData={this.state.booksData} deletBOOK={this.deletBOOK} showbooks={this.state.showbooks} />}
       </Jumbotron>
     )
   }
